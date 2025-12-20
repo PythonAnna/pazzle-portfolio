c3580acbbb93cdc3b4a21f7db2fe2923e48f1a23
PythonAnna <zykova.anna.08@gmail.com>
2025-12-20 14:30:04 +0300

    feat: добавлена практика 13, но её нужно доделать!

---
+++ course-1/practices/Practice13/Practice13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Practice13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Name = "Алиса";
+            p.Age = 25;
+            p.Age = -5;
+        }
+        public class Person
+        {
+            private string name;
+            private int age;
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    if (value != "")
+                    {
+                        name = value;
+                    }
+                    else
+                        Console.WriteLine("Вы не указали имя");
+                }
+            }
+            public int Age
+            {
+                get { return age; }
+                set
+                {
+                    if (value >= 0)
+                    {
+                        age = value;
+                    }
+                    else
+                        Console.WriteLine("Возраст не может быть отрицательным!");
+                }
+            }
+        }
+        public class BankAccount
+        {
+
+        }
+    }
+}
